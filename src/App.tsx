afc093f6170bd743ebffd68a3c540f3dded7e707
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-04-04 20:17:57 +0000

    feat: Set public folder, custom fonts & colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css"
 const App = () => {
   return (
     <main>
-      <p className="text-orange-500 text-2xl font-bold">Nigga balls</p>
+      <p className="text-2xl font-robert-regular text-violet-300">Nigga balls</p>
     </main>
   )
 }
