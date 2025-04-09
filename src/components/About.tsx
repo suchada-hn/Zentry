3f7bfbb625332b50bd2d91d6244a81f2b55e2fae
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-04-09 19:25:44 +0000

    feat: add Animated text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -30,7 +30,7 @@ const About = () => {
             <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
                 <h2 className="font-general text-sm uppercase md:text[10px]">Welcome to Zentry</h2>
 
-                <AnimatedTitle />
+                <AnimatedTitle title="Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure" containerClass="mt-5 !text-black text-center" />
 
                 <div className="about-subtext">
                     <p>The Game of Games begins-your life, now an epic MMORPG</p>
