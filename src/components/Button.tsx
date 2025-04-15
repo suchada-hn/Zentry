9f959229883adcc23d7358eb8f850a40ebfc6630
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-04-15 10:29:47 +0000

    fix: Swirl button animation now working

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,11 @@ const Button: React.FC<ButtonProps> = ({ id, title, rightIcon, leftIcon, contain
         <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
             {leftIcon}
             <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
-                <div>
+                <div className="translate-y-0 skew-y-0 transition duration-500 group-hover:translate-y-[-160%] group-hover:skew-y-12">
+                    {title}
+                </div>
+
+                <div className="absolute translate-y-[164%] skew-y-12 transition duration-500 group-hover:translate-y-0 group-hover:skew-y-0">
                     {title}
                 </div>
             </span>
@@ -20,4 +24,4 @@ const Button: React.FC<ButtonProps> = ({ id, title, rightIcon, leftIcon, contain
     )
 }
 
-export default Button
+export default Button;
